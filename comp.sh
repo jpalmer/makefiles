97d55bfd3508192804ea8707e2e65e6594ea9eb3
John Palmer <jpal8929@physics.usyd.edu.au>
2011-11-28 10:57:50 +1100

    initial commit

---
+++ comp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#compile the latex file given as the first argument
+FILE=$1
+PDFFILE=`echo $FILE | sed 's/.tex/.pdf/g'`
+pdflatex $FILE  || (echo failure ; rm $PDFFILE ; exit 1) #first compile
+bibtex $FILE
+pdflatex $FILE
+#final compile
+Result=`pdflatex $FILE`
+
+echo "--------------------"
+echo "Warnings"
+echo "$Result" | grep -o 'Warning.*\|[a-x]*\.tex' #for multi-line files show which file has the warning
+echo "--------------------"
+pdfinfo $PDFFILE | grep -i pages #note this can be > 40 (some sections are allowed to be longer than  page but only count as 1 - particularly bibliography
+echo "--------------------"
+

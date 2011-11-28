9a192f85762f451bd1988fa9170bc5d7e41cdfe7
John Palmer <jpal8929@physics.usyd.edu.au>
2011-11-28 11:23:35 +1100

    small error in comp.sh

--- comp.sh
+++ comp.sh
@@ -2,8 +2,9 @@
 #compile the latex file given as the first argument
 FILE=$1
 PDFFILE=`echo $FILE | sed 's/.tex/.pdf/g'`
+AUXFILE=`echo $FILE | sed 's/.tex/.aux/g'`
 pdflatex $FILE  || (echo failure ; rm $PDFFILE ; exit 1) #first compile
-bibtex $FILE
+bibtex $AUXFILE
 pdflatex $FILE
 #final compile
 Result=`pdflatex $FILE`

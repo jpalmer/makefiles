e570af25a2df2b09d9c8ea6c1e15df85b3cda63c
John Palmer <jpal8929@physics.usyd.edu.au>
2011-12-05 14:20:06 +1100

    improve latex compile file

--- comp.sh
+++ comp.sh
@@ -3,7 +3,7 @@
 FILE=$1
 PDFFILE=`echo $FILE | sed 's/.tex/.pdf/g'`
 AUXFILE=`echo $FILE | sed 's/.tex/.aux/g'`
-pdflatex $FILE  || (echo failure ; rm $PDFFILE ; exit 1) #first compile
+pdflatex -halt-on-error $FILE  || (echo failure to compile - quitting ; rm $PDFFILE; exit 1) || exit 0; #first compile
 bibtex $AUXFILE
 pdflatex $FILE
 #final compile
@@ -11,7 +11,7 @@ Result=`pdflatex $FILE`
 
 echo "--------------------"
 echo "Warnings"
-echo "$Result" | grep -o 'Warning.*\|[a-x]*\.tex' #for multi-line files show which file has the warning
+echo "$Result" | grep -o 'Warning.*\|Underfull.*\|Overfull.*\|[a-x]*\.tex' #for multi-line files show which file has the warning
 echo "--------------------"
 pdfinfo $PDFFILE | grep -i pages #note this can be > 40 (some sections are allowed to be longer than  page but only count as 1 - particularly bibliography
 echo "--------------------"
